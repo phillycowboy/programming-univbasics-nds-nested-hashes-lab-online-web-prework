55ae297267cf6d08f113419c36093f42eb199aea
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-10-25 20:07:17 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -35,5 +35,6 @@ end
 def aoh_update(aoh, row, key, new_value)
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
-  [1][:name]="Joelle van Dyne"
+  aoh[1][:name]="Joelle van Dyne"
+  aoh
 end
